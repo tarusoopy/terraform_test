e340dd8bcdc15ad04309ae3111af903b3f312a68
h.nakano <h.nakano@variosecure.net>
2020-09-18 18:10:23 +0900

    shellスクリプト呼び出し追加 shellスクリプト呼び出し初期登録

--- instances.tf
+++ instances.tf
@@ -22,6 +22,8 @@ resource "google_compute_instance" "winrmtest-1" {
 
   }
 
+  meta_startup_script = "${file("./test.sh")}"
+
   service_account {
     scopes = ["userinfo-email", "compute-ro", "storage-ro", "bigquery", "monitoring"]
   }
---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "This is test.sh!"
+echo ""
+
+source function_test.sh
